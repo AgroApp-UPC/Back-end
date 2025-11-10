f8267906c779483b64f8b714dd1988187a0ac946
Jose04 <joseunilad04@gmail.com>
2025-11-10 15:24:41 -0500

    Refactor AuditableAbstractAggregateRoot class

--- src/main/java/com/example/agroapp/shared/domain/model/aggregates/AuditableAbstractAggregateRoot.java
+++ src/main/java/com/example/agroapp/shared/domain/model/aggregates/AuditableAbstractAggregateRoot.java
@@ -1,16 +1,32 @@
 package com.example.agroapp.shared.domain.model.aggregates;
 
-import lombok.Data;
-import java.time.LocalDateTime;
 import jakarta.persistence.*;
 import lombok.Getter;
-import java.util.Date;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.domain.AbstractAggregateRoot;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-@Data
-public abstract class AuditableAbstractAggregateRoot {
+import java.util.Date;
 
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public abstract class AuditableAbstractAggregateRoot<T extends AbstractAggregateRoot<T>> extends AbstractAggregateRoot<T> {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private Date createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private Date updatedAt;
+
+
+    public void addDomainEvent(Object event) {
+        super.registerEvent(event);
+    }
 }
